f2eb579683c912d6ae960f93cb51f30418d78ad0
Sarvajeet <sarvajeetshahktn@gmail.com>
2025-05-07 12:21:07 +0530

    response

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,43 +1,46 @@
-import React, { useContext } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { Link, Navigate, useNavigate } from 'react-router-dom'
-import GetAllCertificate from '../components/Dashboard/GetAllCertificate'
-import { AppContext } from '../context/store'
+import React, { useContext } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import GetAllCertificate from "../components/Dashboard/GetAllCertificate";
+import { AppContext } from "../context/store";
+import "../components/Dashboard/dashboard-certificate.css";
 
 const Dashboard = () => {
+  const { isLoading, token, user } = useContext(AppContext);
 
-    const { isLoading, token, user } = useContext(AppContext)
-
-
-    if (isLoading) {
-        return <h1>Loading...</h1>
-    } else {
-        if (!user?.isAdmin) {
-            return <Navigate to="/" />
-        }
-
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  } else {
+    if (!user?.isAdmin) {
+      return <Navigate to="/" />;
     }
+  }
 
+  return (
+    <section className="dashboardContainer">
+      <Container>
+        <Row>
+          <Col className="dashboard">
+            <div className="dashboardLink">
+              <h1>Admin Dashboard</h1>
+              <div>
+                <span>
+                  <Link to={"/dashboard/generate-certificateId"}>
+                    Generate Certificate Id
+                  </Link>
+                </span>
+                <span style={{ marginLeft: "15px" }}>
+                  <Link to={"/search-certificate"}>Search Certificate</Link>
+                </span>
+              </div>
+            </div>
+            <h4>All Certificate</h4>
+            <GetAllCertificate />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-    return (
-        <section className='dashboardContainer'>
-            <Container>
-                <Row>
-                    <Col className="dashboard">
-                        <div>
-                            <h1>Admin Dashboard</h1>
-                            <div>
-                                <span><Link to={"/dashboard/generate-certificateId"}>Generate Certificate Id</Link></span>
-                                <span style={{marginLeft:"15px"}}><Link to={"/search-certificate"}>Search Certificate</Link></span>
-                            </div>
-                        </div>
-                        <h4>All Certificate</h4>
-                        <GetAllCertificate />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- client/src/service/apis.jsx
+++ client/src/service/apis.jsx
@@ -1,29 +1,20 @@
 // const BASE_URL = "http://localhost:4000/api/v1";
 const BASE_URL = "https://ciao-laguno.onrender.com/api/v1";
 
-
-
-
-
 export const auth = {
-    SIGNUP_API: BASE_URL + "/user/signup",
-    LOGIN_API: BASE_URL + "/user/login",
-    USER_DETAILS_API: BASE_URL + "/user/user-details",
-
-}
-
-
+  SIGNUP_API: BASE_URL + "/user/signup",
+  LOGIN_API: BASE_URL + "/user/login",
+  USER_DETAILS_API: BASE_URL + "/user/user-details",
+};
 
 export const certificate = {
-    CREATE_CERTIFICATE_API: BASE_URL + "/certificate/create-certificate",
-    GET_ALL_CERTIFICATES_API: BASE_URL + "/certificate/get-all-certificate",
-    DELETE_CERTIFICATE_API: BASE_URL + "/certificate/delete-certificate",
-    CERTIFICATE_API: BASE_URL + "/certificate",
-
-}
-
+  CREATE_CERTIFICATE_API: BASE_URL + "/certificate/create-certificate",
+  GET_ALL_CERTIFICATES_API: BASE_URL + "/certificate/get-all-certificate",
+  DELETE_CERTIFICATE_API: BASE_URL + "/certificate/delete-certificate",
+  CERTIFICATE_API: BASE_URL + "/certificate",
+};
 
 export const generateCertificateId = {
-    GENERATE_CERTIFICATE_ID_API: BASE_URL + '/generate/generate-certificateId',
-    SEARCH_CERTIFICATE_ID_API: BASE_URL + '/generate/search/certificate'
-}
+  GENERATE_CERTIFICATE_ID_API: BASE_URL + "/generate/generate-certificateId",
+  SEARCH_CERTIFICATE_ID_API: BASE_URL + "/generate/search/certificate",
+};
